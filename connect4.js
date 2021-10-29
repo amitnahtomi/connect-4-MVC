cdd49f39374b794db0ae694c5309a8b3410dc272
amitnahtomi <amit.nahtomi@gmail.com>
2021-10-29 13:40:35 +0300

    game can be played

--- connect4.js
+++ connect4.js
@@ -1,3 +1,79 @@
-class Model {}
-class View {}
-class Controller {}
+class Model {
+    constructor(){
+        this.currentPlayer = 1,
+        this.boardObj = {
+            0: [0, 0, 0, 0, 0, 0, 0],
+            1: [0, 0, 0, 0, 0, 0, 0],
+            2: [0, 0, 0, 0, 0, 0, 0],
+            3: [0, 0, 0, 0, 0, 0, 0],
+            4: [0, 0, 0, 0, 0, 0, 0],
+            5: [0, 0, 0, 0, 0, 0, 0],
+            6: [0, 0, 0, 0, 0, 0, 0]
+        }
+    }
+    playMove(tdId) {
+        let moveArr = tdId.split("-");
+        let row = Number(moveArr[0]);
+        let square = Number(moveArr[1]);
+        if(this.boardObj[row][square] === 0 && (row === 6 || this.boardObj[row + 1][square] !== 0)){
+            this.boardObj[row][square] = this.currentPlayer;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    checkWin(id) {
+        let moveArr = tdId.split("-");
+        let row = Number(moveArr[0]);
+        let square = Number(moveArr[1]); 
+    }
+    switchPlayer() {
+        if(this.currentPlayer === 1){
+            this.currentPlayer = 2;
+        }
+        else {
+            this.currentPlayer = 1;
+        }
+    }
+
+}
+class View {
+    constructor() {
+        this.playingColor = "yellow";
+    }
+    playMove(id) {
+        document.getElementById(id).style.backgroundColor = this.playingColor;
+    }
+    switchPlayer(){
+       if(this.playingColor === "yellow") {
+           this.playingColor = "blue";
+       }
+       else {
+           this.playingColor = "yellow";
+       }
+    }
+    shoWin(player) {
+        document.getElementById("win").innerText = `player ${player} won`;
+    }
+}
+class Controller {
+    constructor(model, view) {
+        this.model = model,
+        this.view = view;
+    }
+    playTheGame() {
+        document.getElementById("board").addEventListener("click", (event) => {
+            if(event.target.tagName !== 'TD') return;
+            if(this.model.playMove(event.target.id) === false) return;
+            this.view.playMove(event.target.id);
+            this.model.switchPlayer();
+            this.view.switchPlayer();
+        })
+    }
+}
+let app = new Controller(new Model, new View);
+app.playTheGame();
+//let model = new Model;
+//model.playMove("6-0")
+//console.log(model.boardObj);
